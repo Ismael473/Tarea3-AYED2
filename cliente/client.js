35d58d1980490a5086ba63089a30f8d4b344d520
Striker2785 <meneito200337@gmail.com>
2023-05-05 23:55:14 -0600

    Final

--- cliente/client.js
+++ cliente/client.js
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
 const { read } = require('fs');
 const net = require('net');
 const readline = require('readline-sync');
-=======
 //Constantes para obtener los valores de los botones oprimidos en la calculadora
 
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-number]')
-const equalsButton = document.querySelectorAll('[data-equals]')
-const deleteButton = document.querySelectorAll('[data-delete]')
-const allClearButton = document.querySelectorAll('[data-all-clear]')
-const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]')
-const actualOperandTextElement= document.querySelectorAll('[data-actual-operand]')
 
 
 const net = require('net');//Se requiere la constante net para la conexión
->>>>>>> 78e7ad465d5eb4990df531a98f867c05129255d4
+
 
 console.log("El cliente se intenta conectar")
 
@@ -41,7 +32,6 @@ client.on('error',(err)=>{
     console.log(err.message)
 })
 
-<<<<<<< HEAD
 function sendLine(){
     var line = readline.question('\nEscriba algo\t')
     if (line == "0"){
@@ -50,6 +40,6 @@ function sendLine(){
         client.write(line)
     }
 }
-=======
 
->>>>>>> 78e7ad465d5eb4990df531a98f867c05129255d4
+
+

78e7ad465d5eb4990df531a98f867c05129255d4
Ismael473 <ismaelmena73@gmail.com>
2023-05-05 23:14:37 -0600

    Mejoras de funcionalidad

--- cliente/client.js
+++ cliente/client.js
@@ -1,8 +1,19 @@
-const net = require('net');
+//Constantes para obtener los valores de los botones oprimidos en la calculadora
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-number]')
+const equalsButton = document.querySelectorAll('[data-equals]')
+const deleteButton = document.querySelectorAll('[data-delete]')
+const allClearButton = document.querySelectorAll('[data-all-clear]')
+const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]')
+const actualOperandTextElement= document.querySelectorAll('[data-actual-operand]')
+
+
+const net = require('net');//Se requiere la constante net para la conexión
 
 console.log("El cliente se intenta conectar")
 
-const options = 
+const options = //almacena el número del puerto y el valor del host
 {
     port: 4000,
     host: '127.0.0.1'
@@ -18,3 +29,5 @@ client.on('connect', ()=>{
 client.on('error',(err)=>{
     console.log(err.message)
 })
+
+
